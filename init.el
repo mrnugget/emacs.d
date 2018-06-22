30b54e7ed327748e34816bd10db0fd25f97e3d3f
Thorsten Ball <mrnugget@gmail.com>
2018-06-22 10:33:10 +0200

    Minor changes

--- init.el
+++ init.el
@@ -1,9 +1,8 @@
-(setq debug-on-error t)
 ;; no toolbar
 (tool-bar-mode -1)
 ;; show parens
-(show-paren-mode t)
 (setq show-paren-delay 0)
+(show-paren-mode t)
 ;; no scroll bar
 (scroll-bar-mode -1)
 ;; show linenumbers
@@ -21,8 +20,9 @@
 (setq ns-use-native-fullscreen nil)
 ;; Always select the opened help window so it's easily closable with "q"
 (setq help-window-select t)
-;; winner mode -- C-c left/right
-(winner-mode t)
+
+(setq x-select-enable-clipboard t)
+
 ;; default font: hack
 (set-face-attribute 'default nil :family "Hack")
 ;; utf-8 & emojis
@@ -59,6 +59,12 @@
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
 ;; Packages
+
+;; winner mode -- C-c left/right
+(winner-mode t)
+(global-set-key (kbd "C-c C-u") 'winner-undo)
+(global-set-key (kbd "C-c C-r") 'winner-redo)
+
 (require 'package)
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
@@ -118,10 +124,10 @@
 (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
 
 ;; projectile
+(setq projectile-switch-project-action 'projectile-dired)
+(setq projectile-completion-system 'ivy)
 (require 'projectile)
 (projectile-mode 1)
-(setq projectile-switch-project-action #'counsel-projectile-find-file)
-(setq projectile-completion-system 'ivy)
 ;; projectile-rails
 (projectile-rails-global-mode 1)
 
@@ -154,6 +160,9 @@
 (global-evil-search-highlight-persist t)
 (evil-commentary-mode 1)
 (evil-mode 1)
+
+;; this should make ivy-occur buffers editable again (https://github.com/syl20bnr/spacemacs/issues/10290)
+(evil-set-initial-state 'ivy-occur-grep-mode 'normal)
 
 ;; allows us to "paste over" things while visually selected
 (fset 'evil-visual-update-x-selection 'ignore)
@@ -196,7 +205,7 @@
 ;; Using binding.pry in rspec: https://emacs.stackexchange.com/questions/3537/how-do-you-run-pry-from-emacs
 (add-hook 'after-init-hook 'inf-ruby-switch-setup)
 ;; treat _ as part of the word in ruby
-(add-hook 'ruby-mode-hook #'(lambda () (modify-syntax-entry ?_ "w")))
+;; (add-hook 'ruby-mode-hook #'(lambda () (modify-syntax-entry ?_ "w")))
 
 ;; go -- taken from here: https://johnsogg.github.io/emacs-golang
 ;; Define function to call when go-mode loads
@@ -251,11 +260,25 @@
 (setq git-commit-summary-max-length 50)
 (setq magit-completing-read-function 'ivy-completing-read)
 
+;; Tramp
+(setq tramp-default-method "ssh")
+
 ;; smooth-scrolling
 (require 'smooth-scrolling)
 (setq scroll-margin 5)
 (setq scroll-conservatively 9999)
 (setq scroll-step 1)
+
+;; allow piping to jq
+
+(defun pipe-to-jq()
+  (interactive)
+  (message region-beginning)
+  (shell-command-on-region
+   (region-beginning) (region-end)
+   "cat | jq ." nil t))
+
+(evil-leader/set-key "jq" 'pipe-to-jq)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -268,8 +291,46 @@
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
  '(custom-enabled-themes nil)
  '(evil-search-module (quote evil-search))
+ '(ibuffer-saved-filter-groups nil)
+ '(ibuffer-saved-filters
+   (quote
+    (("hel"
+      (projectile-files . "/Users/mrnugget/work/triebwerk/"))
+     ("programming"
+      (or
+       (derived-mode . prog-mode)
+       (mode . ess-mode)
+       (mode . compilation-mode)))
+     ("text document"
+      (and
+       (derived-mode . text-mode)
+       (not
+        (starred-name))))
+     ("TeX"
+      (or
+       (derived-mode . tex-mode)
+       (mode . latex-mode)
+       (mode . context-mode)
+       (mode . ams-tex-mode)
+       (mode . bibtex-mode)))
+     ("web"
+      (or
+       (derived-mode . sgml-mode)
+       (derived-mode . css-mode)
+       (mode . javascript-mode)
+       (mode . js2-mode)
+       (mode . scss-mode)
+       (derived-mode . haml-mode)
+       (mode . sass-mode)))
+     ("gnus"
+      (or
+       (mode . message-mode)
+       (mode . mail-mode)
+       (mode . gnus-group-mode)
+       (mode . gnus-summary-mode)
+       (mode . gnus-article-mode))))))
  '(org-agenda-files (quote ("~/org/road_to_emacs.org" "~/org/life.org")))
  '(package-selected-packages
    (quote
-    (color-theme-sanityinc-tomorrow evil-search-highlight-persist org erlang htmlize alchemist edit-indirect projectile-rails go-autocomplete switch-window smooth-scrolling rjsx-mode lenlen-theme projectile-ripgrep evil-visualstar evil-surround evil-commentary wgrep yaml-mode slim-mode evil-magit magit rspec-mode go-mode geiser exec-path-from-shell evil-leader markdown-mode ivy counsel-projectile projectile evil))))
+    (evil-matchit color-theme-sanityinc-tomorrow evil-search-highlight-persist org erlang htmlize alchemist edit-indirect projectile-rails go-autocomplete switch-window smooth-scrolling rjsx-mode lenlen-theme projectile-ripgrep evil-visualstar evil-surround evil-commentary wgrep yaml-mode slim-mode evil-magit magit rspec-mode go-mode geiser exec-path-from-shell evil-leader markdown-mode ivy counsel-projectile projectile evil))))
 
